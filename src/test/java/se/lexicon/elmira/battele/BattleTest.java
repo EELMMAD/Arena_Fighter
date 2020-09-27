83377998d4567dae981296c485e2bb0cebb9e24e
Elmira Madadi <elmiramadadi@gmail.com>
2020-09-27 23:59:14 +0200

    Finished with tests

---
+++ src/test/java/se/lexicon/elmira/battele/BattleTest.java
@@ -0,0 +1,36 @@
+package se.lexicon.elmira.battele;
+
+import org.junit.Test;
+import se.lexicon.elmira.battle.Battle;
+import se.lexicon.elmira.model.Player;
+
+import static org.junit.Assert.*;
+
+public class BattleTest {
+
+    @Test
+    public void given_player_and_maxi_battle_return_false_and_battleLog_notEmpty(){
+        Player player = new Player("player", 1, 3);
+        Player maxi = new Player("maxi", 7, 21);
+
+        Battle battle = new Battle(player, maxi);
+
+        assertFalse(battle.battle());
+        assertFalse(battle.getBattleLog().isEmpty());
+    }
+
+    @Test
+    public void given_player_and_mini_battle_return_true_and_battleLog_and_score_added_to_player(){
+        Player player = new Player("player", 7, 21);
+        Player mini = new Player("mini", 1, 3);
+
+        int expectedPlayerScore = 2;
+
+        Battle battle = new Battle(player, mini);
+
+        assertTrue(battle.battle());
+        assertEquals(expectedPlayerScore, player.getScore());
+
+    }
+
+}
